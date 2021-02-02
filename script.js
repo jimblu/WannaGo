fe975d48a578c4db70b6af083191f0ccdecf67bd
jimblu <jimmy.pezp@gmail.com>
2021-02-03 00:11:59 +0100

    updates

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-function showContent() {
-  document.querySelector(".loader-container").classList.add("hidden");
-}
+// barba.init({
+//   transitions: [
+//     {
+//       name: "opacity-transition",
+//       leave(data) {
+//         return gsap.to(data.current.container, {
+//           opacity: 0,
+//         });
+//       },
+//       enter(data) {
+//         const promise1 = new Promise(() => {
+//           data = true;
+//         });
+//         promise1.then((data) => {
+//           return gsap.from(data.next.container, {
+//             opacity: 0,
+//           });
+//         });
+//       },
+//     },
+//   ],
+// });
 
-setTimeout(showContent, 3000);
+barba.init({
+  transitions: [
+    {
+      name: "opacity-transition",
+      leave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0,
+        });
+      },
+      enter(data) {
+        return gsap.from(data.next.container, {
+          opacity: 0,
+        });
+      },
+    },
+  ],
+});
